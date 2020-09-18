8430c26c76e16c14da9653c689c1712812b48041
Jonathan Myers <submyers@uga.edu>
2020-09-17 20:03:23 -0400

    Adding basics

--- csci6950/settings.py
+++ csci6950/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9+=9u4)gcsjs4t#5(v1p!3jt(+^nle8uzmm)lf13!dfvbogaa&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [localhost]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
 ]
 
 MIDDLEWARE = [
